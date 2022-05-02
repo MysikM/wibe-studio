499a92401dff10a0c924c31bd0e9c825f382ccdb
Mysik <Mishamysik@gmail.com>
2022-05-02 11:37:03 +0300

    mobile size shop

--- src/sections/Shop.jsx
+++ src/sections/Shop.jsx
@@ -38,6 +38,14 @@ const Title = styled.h1`
   top: 1rem;
   left: 5%;
   z-index: 11;
+
+  @media (max-width: 64em) {
+    font-size: ${props => props.theme.fontxxl};
+  }
+
+  @media (max-width: 48em) {
+    font-size: ${props => props.theme.fontxl};
+  }
 `;
 
 const Left = styled.div`
@@ -60,6 +68,23 @@ const Left = styled.div`
     width: 80%;
     margin: 0 auto;
   }
+
+  @media (max-width: 64em) {
+    p{
+      font-size: ${props => props.theme.fontmd};
+    }
+  }
+  @media (max-width: 48em) {
+    width: 40%;
+    p{
+      font-size: ${props => props.theme.fontsm};
+    }
+  }
+  @media (max-width: 30em) {
+    p{
+      font-size: ${props => props.theme.fontxs};
+    }
+  }
 `;
 
 const Right = styled.div`
@@ -77,8 +102,9 @@ const Right = styled.div`
   h1{
     width: 5rem;
     margin: 0 2rem;
-    
   }
+  
+
 `;
 
 const Item = styled(motion.div)`
@@ -101,6 +127,10 @@ const Item = styled(motion.div)`
     font-weight: 500;
     text-align: center;
     cursor: pointer;
+  }
+
+  @media (max-width: 48em){
+    width: 15rem;
   }
 `;
 
